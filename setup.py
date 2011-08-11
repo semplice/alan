e4d39dec25d4cc175f9c6a1ab4a715410977f6e7
g7 <morarossa@gmail.com>
2011-08-11 23:57:21 +0200

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name='alan',
-      version='0.2.1',
+      version='0.3.0',
       description='Openbox Menu Extension Framework',
       author='Eugenio Paolantonio and the Semplice Team',
       author_email='morarossa@gmail.com',
